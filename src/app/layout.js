5305f8cf2a5cacf3c82798097bb0aeafa729cb25
leonardhw <leonardharyow@gmail.com>
2023-07-17 10:01:20 +0700

    fix deploy 1

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,11 +9,11 @@ import { register } from 'swiper/element/bundle';
 
 const quicksand = Quicksand({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'Wealth Management Platform: Capai Tujuan Keuangan Dengan PINA',
-  description:
-    'Dapatkan solusi keuangan terbaik dari PINA, wealth management platform yang menyediakan akses ke financial planner berpengalaman untuk bantu kamu capai tujuan keuangan.',
-};
+// export const metadata = {
+//   title: 'Wealth Management Platform: Capai Tujuan Keuangan Dengan PINA',
+//   description:
+//     'Dapatkan solusi keuangan terbaik dari PINA, wealth management platform yang menyediakan akses ke financial planner berpengalaman untuk bantu kamu capai tujuan keuangan.',
+// };
 
 export default function RootLayout({ children }) {
   register();
